a8fbb8491b983db9846536b4b72eab4b38701882
xiajian <jhqy2011@gmail.com>
2016-06-20 15:45:21 +0800

    try to fix assets file load

--- lib/layer/rails.rb
+++ lib/layer/rails.rb
@@ -1,8 +1,46 @@
-require "layer/rails/version"
+require 'layer/rails/version'
+require 'layer/rails/download'
 
 module Layer
   module Rails
-    class Engine < ::Rails::Engine
+    module RailsAssetsLayer
+
+      def self.gem_path
+        Pathname(File.realpath(__FILE__)).join('../../..')
+      end
+
+      def self.gem_spec
+        Gem::Specification::load(
+            gem_path.join('layer-rails.gemspec').to_s
+        )
+      end
+
+      def self.load_paths
+        gem_path.join('app/assets').each_child.to_a
+      end
+
+      if defined?(Rails)
+        class Engine < ::Rails::Engine
+          # Rails -> use app/assets directory.
+        end
+      end
     end
+
+    class RailsAssets
+      @components ||= []
+
+      class << self
+        attr_accessor :components
+
+        def load_paths
+          components.flat_map(&:load_paths)
+        end
+      end
+    end
+
+    RailsAssets.components << RailsAssetsLayer
   end
 end
+
+
+
---
+++ lib/layer/rails/download.rb
@@ -0,0 +1,12 @@
+module Layer
+  module Rails
+    module Download
+      # 貌似，不能直接从官网下载文件
+      URL = 'http://res.layui.com/download/layer-v2.3.zip'
+
+      class << self
+
+      end
+    end
+  end
+end
